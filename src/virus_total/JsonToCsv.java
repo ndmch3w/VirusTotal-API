ac3919106f5907decefe7109d9fdb34bbd16e75b
ndmch3w <daiminh.ng.cs@gmail.com>
2023-05-29 13:02:00 +0700

    Trying to complete JsonToCsv

--- src/virus_total/JsonToCsv.java
+++ src/virus_total/JsonToCsv.java
@@ -27,11 +27,19 @@ public class JsonToCsv {
         FileReader fileReader = new FileReader(jsonFilePath);
 
         JsonObject jsonObject = gson.fromJson(fileReader, JsonObject.class);
+
         JsonObject statsObject = jsonObject
                 .getAsJsonObject("data")
                 .getAsJsonObject("attributes")
                 .getAsJsonObject("stats");
-        System.out.println("New JSON: " + statsObject.toString());
+        System.out.println("Overall report (new Json): " + statsObject.toString());
+
+        JsonObject resultsObject = jsonObject
+                .getAsJsonObject("data")
+                .getAsJsonObject("attributes")
+                .getAsJsonObject("results");
+        System.out.println("Detailed report (new Json): " + resultsObject.toString());
+
     }
     public static void main(String[] args) throws IOException{
         JsonToCsv test = new JsonToCsv("FileReport.json");
